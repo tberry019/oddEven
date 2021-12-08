d2e91010b844bd7419cceea0b8f33cc5d5677c07
Taylor Berry <taylorberry019@gmail.com>
2021-12-08 09:34:28 -0700

    oddEven

---
+++ app.js
@@ -0,0 +1,12 @@
+//INPUT: number
+function evenOrOdd(num) {
+  // div by 2 / multiple by 2
+
+  // how to chck if there is remainder?
+  return num % 2 === 0
+
+
+
+}
+// OUTPUT: Boolean (true/false)
+
